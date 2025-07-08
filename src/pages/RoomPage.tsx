047f3e0102cee26b278c08bf0a0693bca125d8c4
Hyeseong <sdgoh0905@gmail.com>
2025-07-08 22:50:32 +0900

    feat: create RoomPage without WebRTC

--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import Background from '../components/Background';
+
 function RoomPage() {
-  return 'RoomPage';
+  return (
+    <div className="relative min-h-screen bg-amber-100">
+      <Background size="large">
+        <div className="absolute top-6 right-16">
+          <Link
+            to="/main"
+            className="px-8 py-1.5 rounded-lg bg-rose-200 hover:bg-rose-300 text-rose-800"
+          >
+            나가기
+          </Link>
+        </div>
+      </Background>
+    </div>
+  );
 }
 
 export default RoomPage;
